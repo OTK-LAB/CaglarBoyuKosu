cde1749b1ddfab5b7b46c00ec37365dabf56f85a
THEPHATIH\fatih <fatih.ates@ogr.iuc.edu.tr>
2022-12-03 14:39:44 +0300

    1.hafta -> Level geçişi eklendi.

---
+++ Assets/loadnewlevel.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadnewlevel : MonoBehaviour
+{
+    public int iLevelToLoad;
+    public string sLevelToLoad;
+
+    public bool useIntegerToLoadLevel = false;
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+
+        if(collisionGameObject.name == "Player")
+        {
+            LoadScene();
+        }
+    }
+    void LoadScene()
+    {
+        if(useIntegerToLoadLevel)
+        {
+            SceneManager.LoadScene(iLevelToLoad);
+        }
+        else
+        {
+            SceneManager.LoadScene(sLevelToLoad);
+        }
+    }
+}

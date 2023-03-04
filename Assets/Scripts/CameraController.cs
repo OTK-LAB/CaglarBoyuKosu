7c2cfb942d671efe150add5a05ebda788d7d1a98
THEPHATIH\fatih <fatih.atese@yahoo.com>
2023-03-04 16:10:50 +0300

    Kamera kontrolü düzenlendi. Havadayken hareket engellendi.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-public GameObject Player;
-Vector3 offset;
+    public GameObject Player;
+    private Vector3 offset;
+    private bool followPlayer = true;
+    private float timer = 0f;
+    private float followDelay = 0.49f;
 
-private void Start(){
-    offset = transform.position - Player.transform.position;
-}
+    private void Start()
+    {
+        offset = transform.position - Player.transform.position;
+    }
 
-private void LateUpdate(){
-    transform.position = Player.transform.position + offset;
+    private void LateUpdate()
+    {
+        if (followPlayer)
+        {
+            transform.position = Player.transform.position + offset;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+            if (timer >= followDelay)
+            {
+                followPlayer = true;
+                timer = 0f;
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, Player.transform.position.z + offset.z);
+            }
+        }
+    }
 
-}
-
-
-
+    public void StopFollowingPlayer()
+    {
+        followPlayer = false;
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,61 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float jumpForce = 7f;
     public Rigidbody rigid;
 
     public LayerMask groundLayer;
     public float raycastDistance = 0.6f;
-    
-    private bool isGrounded;
 
-    public void start()
+    private bool isGrounded;
+    public bool isJumping;
+
+    private bool canMove = true; // Yeni bir değişken ekledik
+
+    public void Start()
     {
         rigid = gameObject.GetComponent<Rigidbody>();
     }
-            
-    void FixedUpdate(){
-            GetComponent<Rigidbody>().velocity = new Vector3(0,0,10);
+
+    void FixedUpdate()
+    {
+        if (canMove) // Eğer hareket edebilirsek karakteri hareket ettiriyoruz
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 10);
+        }
+        else
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
     }
 
-
-    void Update(){
-        if(Input.GetKey("a")){
-            GetComponent<Rigidbody>().AddForce(-50, 0, 0, ForceMode.Force);
-        }
-
-        else if(Input.GetKey("d")){
-            GetComponent<Rigidbody>().AddForce(50, 0, 0, ForceMode.Force);
+    void Update()
+    {
+        if (canMove && isGrounded) // Eğer hareket edebilirsek ve yerdeysek karakteri hareket ettiriyoruz
+        {
+            if (Input.GetKey("a"))
+            {
+                GetComponent<Rigidbody>().AddForce(-50, 0, 0, ForceMode.Force);
+            }
+            else if (Input.GetKey("d"))
+            {
+                GetComponent<Rigidbody>().AddForce(50, 0, 0, ForceMode.Force);
+            }
         }
 
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance, groundLayer))
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance, groundLayer))
             isGrounded = true;
         else
             isGrounded = false;
 
-        if(Input.GetKeyDown(KeyCode.W) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
             rigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isJumping = true;
+            Camera.main.GetComponent<CameraController>().StopFollowingPlayer();
         }
-
     }
-    
-    
-    
 
-        
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+            canMove = true; // Yere iner inmez tekrar hareket etmemizi sağlıyoruz
+        }
+    }
 
-    void OnTriggerEnter(Collider other){
-        if(other.gameObject.tag =="puan"){
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") && !isJumping)
+        {
+            Camera.main.GetComponent<CameraController>().enabled = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "puan")
+        {
             Destroy(other.gameObject);
         }
-
     }
-
 }
